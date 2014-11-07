b6dd7b5bc15a36b146b038adad1bd490e42131fb
Oisin <oisin@blocktrail.com>
2014-11-07 15:59:31 +0100

    segment and re-write tests definitions

--- test/api_client.test.js
+++ test/api_client.test.js
@@ -9,8 +9,8 @@ var client = blocktrail({
     apiSecret : "MY_APISECRET"
 });
 
-module.exports = {
-    'test Coin Value': function(cb) {
+describe('SDK general', function() {
+    it('test Coin Value', function(cb) {
         assert.equal(blocktrail.toSatoshi(0.00000001), 1);
         assert.equal(blocktrail.toBTC(100000000), 1.0);
 
@@ -18,11 +18,11 @@ module.exports = {
         assert.equal(blocktrail.toBTC(123456789), 1.23456789);
 
         cb();
-    },
-    'test auth failure': function(cb) {
+    });
+    it('test auth failure', function(cb) {
         var client = blocktrail({
-            apiKey : "TESTKEY-FAIL",
-            apiSecret : "TESTSECRET-FAIL"
+            apiKey: "TESTKEY-FAIL",
+            apiSecret: "TESTSECRET-FAIL"
         });
 
         client.address("1dice8EMZmqKvrGE4Qc9bUFf9PX3xaYDp", function(err, address) {
@@ -30,8 +30,10 @@ module.exports = {
 
             cb();
         });
-    },
-    'test address': function(cb) {
+    });
+});
+describe('data api', function() {
+    it('test address', function(cb) {
         client.address("1dice8EMZmqKvrGE4Qc9bUFf9PX3xaYDp", function(err, address) {
             assert.ifError(err);
             assert.ok(address['address']);
@@ -39,8 +41,8 @@ module.exports = {
 
             cb();
         });
-    },
-    'test addressTransactions': function(cb) {
+    });
+    it('test addressTransactions', function(cb) {
         client.addressTransactions("1dice8EMZmqKvrGE4Qc9bUFf9PX3xaYDp", {limit: 23}, function(err, address_txs) {
             assert.ifError(err);
             assert.ok(address_txs['data']);
@@ -49,8 +51,8 @@ module.exports = {
 
             cb();
         });
-    },
-    'test addressUnconfirmedTransactions': function(cb) {
+    });
+    it('test addressUnconfirmedTransactions', function(cb) {
         client.addressUnconfirmedTransactions("1dice8EMZmqKvrGE4Qc9bUFf9PX3xaYDp", {limit: 23}, function(err, address_txs) {
             assert.ifError(err);
             assert.ok('data' in address_txs);
@@ -59,8 +61,8 @@ module.exports = {
 
             cb();
         });
-    },
-    'test addressUnspentOutputs': function(cb) {
+    });
+    it('test addressUnspentOutputs', function(cb) {
         client.addressUnspentOutputs("1dice8EMZmqKvrGE4Qc9bUFf9PX3xaYDp", {limit: 23}, function(err, address_utxo) {
             assert.ifError(err);
             assert.ok('data' in address_utxo);
@@ -69,16 +71,16 @@ module.exports = {
 
             cb();
         });
-    },
-    'test verifyAddress': function(cb) {
+    });
+    it('test verifyAddress', function(cb) {
         client.verifyAddress("16dwJmR4mX5RguGrocMfN9Q9FR2kZcLw2z", "HPMOHRgPSMKdXrU6AqQs/i9S7alOakkHsJiqLGmInt05Cxj6b/WhS7kJxbIQxKmDW08YKzoFnbVZIoTI2qofEzk=", function(err, result) {
             assert.ifError(err);
             assert.ok(result);
 
             cb();
         });
-    },
-    'test block by hash': function(cb) {
+    });
+    it('test block by hash', function(cb) {
         client.block("000000000000034a7dedef4a161fa058a2d67a173a90155f3a2fe6fc132e0ebf", function(err, block) {
             assert.ifError(err);
             assert.ok(block['hash']);
@@ -86,8 +88,8 @@ module.exports = {
 
             cb();
         });
-    },
-    'test block by height': function(cb) {
+    });
+    it('test block by height', function(cb) {
         client.block(200000, function(err, block) {
             assert.ifError(err);
             assert.ok(block['hash']);
@@ -95,8 +97,8 @@ module.exports = {
 
             cb();
         });
-    },
-    'test blockTransactions': function(cb) {
+    });
+    it('test blockTransactions', function(cb) {
         client.blockTransactions("000000000000034a7dedef4a161fa058a2d67a173a90155f3a2fe6fc132e0ebf", {limit: 23}, function(err, block_txs) {
             assert.ifError(err);
             assert.ok(block_txs['data']);
@@ -105,8 +107,8 @@ module.exports = {
 
             cb();
         });
-    },
-    'test allBlocks': function(cb) {
+    });
+    it('test allBlocks', function(cb) {
         client.allBlocks({page:2, limit: 23, sort_dir: 'asc'}, function(err, blocks) {
             assert.ifError(err);
             assert.ok(blocks['data']);
@@ -117,16 +119,16 @@ module.exports = {
 
             cb();
         });
-    },
-    'test blockLatest': function(cb) {
+    });
+    it('test blockLatest', function(cb) {
         client.blockLatest(function(err, block) {
             assert.ifError(err);
             assert.ok(block['hash']);
 
             cb();
         });
-    },
-    'test coinbase transaction': function(cb) {
+    });
+    it('test coinbase transaction', function(cb) {
         client.transaction("4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b", function(err, tx) {
             assert.ifError(err);
             assert.equal(tx['hash'], "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b");
@@ -134,8 +136,8 @@ module.exports = {
 
             cb();
         });
-    },
-    'test random transaction #1': function(cb) {
+    });
+    it('test random transaction #1', function(cb) {
         client.transaction("c791b82ed9af681b73eadb7a05b67294c1c3003e52d01e03775bfb79d4ac58d1", function(err, tx) {
             assert.ifError(err);
             assert.equal(tx['hash'], "c791b82ed9af681b73eadb7a05b67294c1c3003e52d01e03775bfb79d4ac58d1");
@@ -145,8 +147,8 @@ module.exports = {
 
             cb();
         });
-    }
-};
+    });
+});
 
 
 describe('webhooks api', function() {
