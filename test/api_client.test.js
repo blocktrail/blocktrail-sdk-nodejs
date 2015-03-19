d103b6fe1942de8838ab414e59a2b1044fe97947
Ruben de Vries <ruben@rubensayshi.com>
2015-03-19 19:36:31 +0100

    stop testing with genesis transaction

--- test/api_client.test.js
+++ test/api_client.test.js
@@ -132,9 +132,9 @@ describe('data api', function () {
         });
     });
     it('test coinbase transaction', function (cb) {
-        client.transaction("4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b", function (err, tx) {
+        client.transaction("0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098", function (err, tx) {
             assert.ifError(err);
-            assert.equal(tx['hash'], "4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b");
+            assert.equal(tx['hash'], "0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098");
             assert.equal(tx['enough_fee'], null);
 
             cb();
