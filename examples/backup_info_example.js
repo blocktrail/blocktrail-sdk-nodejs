7f8800253d4bd7c3c18dcd5ca2d85559a9862d2e
Ruben de Vries <ruben@rubensayshi.com>
2015-04-01 18:04:30 +0200

    seperate backup_generator for nodejs and browser

--- examples/backup_info_example.js
+++ examples/backup_info_example.js
@@ -6,9 +6,9 @@ var path = require('path');
 var LIBPATH = path.normalize(__dirname + '/..');
 
 var client = blocktrail.BlocktrailSDK({
-    apiKey : "MY_APIKEY",
-    apiSecret : "MY_APISECRET",
-    testnet : true
+    apiKey: "MY_APIKEY",
+    apiSecret: "MY_APISECRET",
+    testnet: true
 });
 
 //create a new wallet
@@ -19,21 +19,22 @@ client.createNewWallet(walletIdentifier, "example-strong-password", 9999, functi
     }
 
     //generate the backup document
-    var backup = new blocktrail.BackupGenerator(primaryMnemonic, backupMnemonic, blocktrailPubKeys);
+    var backup = new blocktrail.BackupGenerator(wallet.identifier, primaryMnemonic, backupMnemonic, blocktrailPubKeys);
     //create a pdf
     backup.generatePDF(LIBPATH + "/examples/my-wallet-backup.pdf", function (err, result) {
         console.log(err, result);
     });
 
     //can also be html or an image
-    var result = backup.generateHTML();
-    fs.writeFile(LIBPATH + "/examples/my-wallet-backup.html", result, function (err) {
-         if (err) {
-             console.log(err);
-         } else {
-             console.log("The file was saved!");
-         }
-     });
+    backup.generateHTML(function(result) {
+        fs.writeFile(LIBPATH + "/examples/my-wallet-backup.html", result, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log("The file was saved!");
+            }
+        });
+    });
 
     //image (only png)
     backup.generateImage(LIBPATH + "/examples/my-wallet-backup.png", function (err, result) {
