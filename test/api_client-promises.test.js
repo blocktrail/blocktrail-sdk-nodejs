255decab852ac2ac42ea41d6408cd6f058854c67
Ruben de Vries <ruben@rubensayshi.com>
2015-04-07 16:33:30 +0200

    fixed jscs issues

--- test/api_client-promises.test.js
+++ test/api_client-promises.test.js
@@ -10,8 +10,8 @@ var client = blocktrail.BlocktrailSDK({
     apiSecret : process.env.BLOCKTRAIL_SDK_APISECRET || "EXAMPLE_BLOCKTRAIL_SDK_NODEJS_APISECRET"
 });
 
-describe("using promises", function() {
-    it ("should work for address request", function(cb) {
+describe("using promises", function () {
+    it ("should work for address request", function (cb) {
         client.address("1dice8EMZmqKvrGE4Qc9bUFf9PX3xaYDp").then(function (address) {
             assert.ok(address['address']);
             assert.equal(address['address'], '1dice8EMZmqKvrGE4Qc9bUFf9PX3xaYDp');
@@ -21,7 +21,7 @@ describe("using promises", function() {
         .done();
     });
 
-    it("should work for verify address request", function(cb) {
+    it("should work for verify address request", function (cb) {
         client.verifyAddress("16dwJmR4mX5RguGrocMfN9Q9FR2kZcLw2z", "HPMOHRgPSMKdXrU6AqQs/i9S7alOakkHsJiqLGmInt05Cxj6b/WhS7kJxbIQxKmDW08YKzoFnbVZIoTI2qofEzk=")
             .then(function (result) {
                 assert.ok(result);
