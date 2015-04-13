a155a109d5c7475a3c923900677b14c403edf33d
Oisin <oisin@blocktrail.com>
2015-04-13 11:21:41 +0200

    code cleanup

--- examples/wallet_recovery_example.js
+++ examples/wallet_recovery_example.js
@@ -34,105 +34,32 @@ var bitcoinDataClient = new blocktrail.BlocktrailBitcoinService({
 
 
 //create instance of sweeper - will automatically create primary keys from mnemonics
-var options = {
+var sweeperOptions = {
     network: 'btc',
     testnet: true,
     logging: true,
-    sweepBatchSize: 3
+    sweepBatchSize: 10
 };
-var walletSweeper = new blocktrail.WalletSweeper(primaryMnemonic, primaryPassphrase, backupMnemonic, blocktrailKeys, bitcoinDataClient, options);
+var walletSweeper = new blocktrail.WalletSweeper(primaryMnemonic, primaryPassphrase, backupMnemonic, blocktrailKeys, bitcoinDataClient, sweeperOptions);
 
 
-//console.log(walletSweeper.createAddress("M/0'/0/0"));         //create address for path
-//console.log(walletSweeper.createBatchAddresses(0, 100, 0));   //create batch address
 
-//find utxos for a single address
-/*
-var address = "mrj2K6txjo2QBcSmuAzHj4nD1oXSEJE1Qo";
-bitcoinDataClient.getUnspentOutputs(address, function(err, result) {
-    console.log("cb:", result.length);
-}).done(function(result) {
-    console.log("Q:", result);
-});
-*/
-
-/*
-//find utxos for a multiple addresses
-var addresses = [
-    "2NEBR7iL1sea6r92Xttk7tNKHsRpFNZBVNm",
-    "2Mu56Tt6AM2hLtJ5qbDYq1PS4hS5pAB4QKH",
-    "2N6tNRzGnPE4cfJxjWYcfsL2E9yrTMXsgfy",
-    "2N7QpVd8n5kJXYrKr1okenFr3E9UddRTz3f",
-    "2NDyAgg7ew1cJazEEymXs734J7VMCyZaLsf",
-    "2N1Y2ey7dc985cBxDhPdDNycVegKxfTk4Ld",
-    "2N9h8UtSvj6ikKGHQWvrKq3rFATAnQM66po",
-    "2N3RxDUF7RyyyQqUAeB3befwCGpnEjQE5NU",
-    "2N2t4sfeh2LHbviT6Cf6PBkAY8u4hmXBza6",
-    "2MsvsvmRrHxEQ8V5XGC6U7xHYbZmbcZBtpL"
-];
-var unspentOutputFinder = require("../lib/unspent_output_finder");
-var finder = new unspentOutputFinder(bitcoinDataClient, {logging:true});
-finder.getUTXOs(addresses).done(function(result) {
-    console.log("Utxos:", result);
-}, function(err) {
-    console.log("error", err);
-});
-*/
-
-
-/*
 //Do wallet fund discovery - can be run separately from sweeping
-walletSweeper.discoverWalletFunds(10).done(function(result) {
+console.log('-----Discovering Funds-----');
+var batchSize = 10;
+walletSweeper.discoverWalletFunds(batchSize).done(function(result) {
     console.log(result);
 }, function(err) {
     console.log(err);
 });
-*/
+
 
 
 //Do wallet fund discovery and sweeping - if successful you will be returned a signed transaction ready to submit to the network
-var receivingAddress = "2N2t4sfeh2LHbviT6Cf6PBkAY8u4hmXBza6";
+console.log('\n-----Sweeping Wallet-----');
+var receivingAddress = "2NCfSZa6f8YwAnjvGcorGDdMSyY9kMzQTZe";
 walletSweeper.sweepWallet(receivingAddress).done(function(result) {
     console.log(result);
 }, function(err) {
     console.log(err);
 });
-//$receivingAddress = "2NCcm7hJfJ5wk6GyKvT2ZHCrNsBgjBv2MSF";
-//$result = $walletSweeper.sweepWallet($receivingAddress);
-//console.log($result);
-
-
-
-/*
-
-//create a new wallet
-var walletIdentifier = "nodejs-example-" + crypto.randomBytes(24).toString('hex');
-client.createNewWallet(walletIdentifier, "example-strong-password", 9999, function (err, wallet, primaryMnemonic, backupMnemonic, blocktrailPubKeys) {
-    if (err) {
-        return console.log("createNewWallet ERR", err);
-    }
-
-    //generate the backup document
-    var backup = new blocktrail.BackupGenerator(primaryMnemonic, backupMnemonic, blocktrailPubKeys);
-    //create a pdf
-    backup.generatePDF(LIBPATH + "/examples/my-wallet-backup.pdf", function (result) {
-        console.log(result);
-    });
-
-    //can also be html or an image
-    var result = backup.generateHTML();
-    fs.writeFile(LIBPATH + "/examples/my-wallet-backup.html", result, function (err) {
-         if (err) {
-             console.log(err);
-         } else {
-             console.log("The file was saved!");
-         }
-     });
-
-    //image (only png)
-    backup.generateImage(LIBPATH + "/examples/my-wallet-backup.png", function (result) {
-        console.log(result);
-    });
-
-});
-*/
