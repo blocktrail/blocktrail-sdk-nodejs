fc24e940a040da271b0856b8c0ec35d370572dc2
Thomas Kerin <thomas@blockcorp.com>
2016-09-19 15:48:52 +0200

    Re-add test run html file

---
+++ test.js
@@ -0,0 +1,6 @@
+exports = module.exports = {
+    api_client: require('./test/api_client.test'),
+    api_client_promises: require('./test/api_client-promises.test'),
+    cross_platform: require('./test/cross_platform.test'),
+    wallet: require('./test/wallet.test')
+};
