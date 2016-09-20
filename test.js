b9c31ecbedf74487d10901b4af22337c59e37ba5
Thomas Kerin <thomas@blockcorp.com>
2016-09-20 15:15:12 +0200

    Gitignore: should be test/run-tests.html Remove debugging output Remove cross_platform test, doesn't test anything not already covered

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 exports = module.exports = {
     api_client: require('./test/api_client.test'),
     api_client_promises: require('./test/api_client-promises.test'),
-    cross_platform: require('./test/cross_platform.test'),
     wallet: require('./test/wallet.test')
 };
