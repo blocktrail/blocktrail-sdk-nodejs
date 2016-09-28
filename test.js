f457aa1d2de5930f36c8552ca7cc29a3c5a917bd
Thomas Kerin <me@thomaskerin.io>
2016-09-28 14:58:25 +0200

    Add V3 wallet encryption scheme. Add sanity check to mnemonic.js, and remove test vectors which violate the rule.

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 exports = module.exports = {
     api_client: require('./test/api_client.test'),
     api_client_promises: require('./test/api_client-promises.test'),
+    crypto: require('./test/crypto.test'),
     wallet: require('./test/wallet.test')
 };
