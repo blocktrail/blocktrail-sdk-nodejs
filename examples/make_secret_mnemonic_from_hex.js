1d02c13f1176db103e0ae682a4a3f817d27dc9b3
Tobias Hoffmann <lacksfish@gmail.com>
2017-08-10 16:55:11 +0200

    make mnemonic from hex example

---
+++ examples/make_secret_mnemonic_from_hex.js
@@ -0,0 +1,19 @@
+var m = require('./../lib/encryption_mnemonic.js');
+var q = require('q')
+var stdin = process.openStdin();
+
+stdin.addListener("data", function(EPS) {
+    // note: EPS is an object, and when converted to a string it will
+    // end with a linefeed.  so we (rather crudely) account for that
+    // with toString() and then trim()
+    var hex = EPS.toString().trim();
+    console.log(hex);
+    var buffer = Buffer.from(hex, 'hex');
+    var mnemonic = m.encode(buffer);
+    var lastTwo = mnemonic.split(" ").slice(-2);
+    console.log(buffer);
+    console.log(buffer.toString('hex'));
+    console.log(mnemonic);
+    console.log(lastTwo);
+    process.exit(0);
+});
