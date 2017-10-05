85470c2d8c5f94a669aec3e21fb6997f1a42c724
Thomas Kerin <thomas.kerin@bitmaintech.com>
2017-10-05 15:36:18 +0200

    more tests

--- test/size_estimation.test.js
+++ test/size_estimation.test.js
@@ -1,6 +1,5 @@
 /* jshint -W101, -W098 */
 /* global window */
-var _ = require('lodash');
 var assert = require('assert');
 var bitcoin = require('bitcoinjs-lib');
 var SizeEstimation = require("../lib/size_estimation");
@@ -38,7 +37,7 @@ var scriptDataLenFixtures =  [
     [65536, 5]
 ];
 
-var multisigFixtures = (function () {
+var multisigStackFixtures = (function () {
     var u = ['5KW8Ymmu8gWManGggZZQJeX7U3pn5HtcqqsVrNUbc1SUmVPZbwp',
         '5KCV94YBsrJWTdk6cQWJxEd25sH8h1cGTpJnCN6kLMLe4c3QZVr',
         '5JUxGateMWVBsBQkAwSRQLxyaQXhsch4EStfC62cqdEf2zUheVT'
@@ -70,6 +69,36 @@ var multisigFixtures = (function () {
     return fixtures;
 })();
 
+// test fixtures of every possible representation of multisig
+var multisigFormFixtures = (function () {
+    var c = ['L1Tr4rPUi81XN1Dp48iuva5U9sWxU1eipgiAu8BhnB3xnSfGV5rd',
+        'KwUZpCvpAkUe1SZj3k3P2acta1V1jY8Dpuj71bEAukEKVrg8NEym',
+        'Kz2Lm2hzjPWhv3WW9Na5HUKi4qBxoTfv8fNYAU6KV6TZYVGdK5HW'
+    ];
+    var keys = c.map(function (wif) {
+        return bitcoin.ECPair.fromWIF(wif, bitcoin.networks.bitcoin).getPublicKeyBuffer();
+    });
+    var multisig = bitcoin.script.multisig.output.encode(2, keys);
+    var bareSize = 1/*op0*/ + 2*(1+SizeEstimation.SIZE_DER_SIGNATURE);
+
+    var bareSig = 1 + bareSize;
+    var p2shSig = 3 + bareSize + 2 + multisig.length;
+
+    var p2wshHash = bitcoin.crypto.sha256(multisig);
+    var p2wshScript = bitcoin.script.witnessScriptHash.output.encode(p2wshHash);
+
+    var nestedSig = 1 + 1 + p2wshScript.length;
+    var witSize = 1 + 2*(1+SizeEstimation.SIZE_DER_SIGNATURE);
+    var nestedWit = witSize + 1 + 1 + multisig.length;
+
+    return [
+        [multisig, false, null, null, bareSig, 0],
+        [multisig, false, multisig, null, p2shSig, 0],
+        [multisig, true, null, multisig, 1, nestedWit],
+        [multisig, true, p2wshScript, multisig, nestedSig, nestedWit]
+    ];
+})();
+
 varIntFixtures.map(function (fixture) {
     var inputLen = fixture[0];
     var expectSize = fixture[1];
@@ -95,7 +124,7 @@ scriptDataLenFixtures.map(function (fixture) {
 });
 
 describe("estimateMultisigStackSize", function () {
-    multisigFixtures.map(function (fixture) {
+    multisigStackFixtures.map(function (fixture) {
         it("works for multisig scripts with the keys", function () {
             var script = fixture[0];
             assert.ok(bitcoin.script.multisig.output.check(script));
@@ -114,6 +143,33 @@ describe("estimateMultisigStackSize", function () {
             for (var i = 0; i < decoded.m; i++) {
                 assert.equal(SizeEstimation.SIZE_DER_SIGNATURE, stackSizes[1+i]);
             }
+        })
+    });
+
+    multisigFormFixtures.map(function (fixture) {
+        it("deals with different representations", function () {
+            var script = fixture[0];
+            var isWit = fixture[1];
+            var rs = fixture[2];
+            var ws = fixture[3];
+            var expectSig = fixture[4];
+            var expectWit = fixture[5];
+
+            assert.ok(bitcoin.script.multisig.output.check(script));
+            var multisig = bitcoin.script.multisig.output.decode(script);
+
+            var stackEst = SizeEstimation.estimateMultisigStackSize(multisig.m, multisig.pubKeys);
+            var stackSizes = stackEst[0];
+
+            var est = SizeEstimation.estimateStackSignatureSize(stackSizes, isWit, rs, ws);
+            assert.equal("object", typeof est);
+            assert.equal(2, est.length);
+
+            var foundSigSize = est[0];
+            var foundWitSize = est[1];
+
+            assert.equal(foundSigSize, expectSig);
+            assert.equal(foundWitSize, expectWit);
         })
     });
 });
